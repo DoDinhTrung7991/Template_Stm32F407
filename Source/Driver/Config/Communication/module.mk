8046ebbc48ead04586326117d03dec94c082c2a5
Do Dinh Trung <dodinhtrung1997@gmail.com>
2025-09-09 01:22:07 -0400

    Update makefiles

--- Source/Driver/Config/Communication/module.mk
+++ Source/Driver/Config/Communication/module.mk
@@ -1,6 +1,6 @@
-UART_MODULE = UART
-Communication_PATH = $(PRJ_PATH)Driver/Config/Communication/
-UART_PATH = $(Communication_PATH)$(UART_MODULE)/
+UART_DIR = UART/
+Communication_PATH = $(SRC_PATH)Driver/Config/Communication/
+UART_PATH = $(Communication_PATH)$(UART_DIR)
 Communication_OBJ_PATH = $(BUILD_RESULT_PATH_DRIVER)Communication/
 Communication_INC = \
 	-I$(UART_PATH) \
